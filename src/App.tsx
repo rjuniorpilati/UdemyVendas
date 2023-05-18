02bea157bb4825d9d24216a9e56a7035d17bdd51
Raimundo Junior <rjunior.pilati@gmail.com>
2023-05-17 22:22:54 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Testando</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
