e20357f1ece11a0e4a8d6e40ff4113b84ce5f2c5
Raimundo Junior <rjunior.pilati@gmail.com>
2023-05-18 15:49:13 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { SafeAreaView, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: #1e2e48;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <View>
         <Text>Testando</Text>
+        <TextNew>NOVO TESTE</TextNew>
       </View>
     </SafeAreaView>
   );
